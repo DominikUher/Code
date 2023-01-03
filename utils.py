b7c17d2b655f7875302e3e3b6cf73ea960e47577
Dominik Uher <dominik.uher@me.com>
2023-01-03 14:53:36 +0100

    New methods for getting data

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import math
 
 def compute_euclidean_distance_matrix(locations):
@@ -14,3 +15,16 @@ def compute_euclidean_distance_matrix(locations):
                     math.hypot((from_node[0] - to_node[0]),
                                (from_node[1] - to_node[1]))))
     return distances
+
+def get_nodes(city):
+    return pd.read_csv(f'./instances/{city}.nodes', sep=' ')
+
+def get_routes(city):
+    return pd.read_csv(f'./instances/{city}.routes', sep=' ')
+
+def get_distance_matrix_from_routes(routes, num_nodes, dist_type):
+    # Returns distance matrix computed from custom .routes format
+    valid = ['Total, Inside, Outside']
+    if dist_type not in valid:
+        raise ValueError(f"{dist_type} is not a valid distance type.\nAcceptable values are: {valid}")
+    return list(zip(*([iter(routes['DistanceInside[km]'])]*num_nodes)))
